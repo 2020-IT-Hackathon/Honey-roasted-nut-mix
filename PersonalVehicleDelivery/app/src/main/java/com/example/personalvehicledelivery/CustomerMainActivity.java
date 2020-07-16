9f6a607ec6c2780ab9104d099e6d3b3ce252abc7
Michael Mao <jmao@gatech.edu>
2020-07-16 15:30:54 -0400

    Modified db structure, added signout button to customer home page.

--- PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/CustomerMainActivity.java
+++ PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/CustomerMainActivity.java
@@ -2,13 +2,31 @@ package com.example.personalvehicledelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CustomerMainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button buttonLogout = findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(CustomerMainActivity.this, MainActivity.class));
+            }
+        });
     }
 }
