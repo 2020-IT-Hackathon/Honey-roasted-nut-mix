02f0946df2a743baaa86eb3cd81cea5be9b0fe30
Michael Mao <jmao@gatech.edu>
2020-07-16 12:43:18 -0400

    Added welcome message for business owners.

--- PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/BusinessMainActivity.java
+++ PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/BusinessMainActivity.java
@@ -1,17 +1,30 @@
 package com.example.personalvehicledelivery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class BusinessMainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference mDatabase;
+    TextView textViewTitle;
+
+    private static final String USERS = "users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +32,27 @@ public class BusinessMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_business_main);
 
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        mDatabase = database.getReference(USERS);
 
+        String uid = mAuth.getCurrentUser().getUid();
+        DatabaseReference ref = mDatabase.child(uid);
+
+        textViewTitle = findViewById(R.id.textViewTitle);
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String businessName = snapshot.child("business_name").getValue().toString();
+                textViewTitle.setText("Welcome, " + businessName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        ref.addListenerForSingleValueEvent(valueEventListener);
         Button buttonLogout = findViewById(R.id.buttonLogout);
         buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
