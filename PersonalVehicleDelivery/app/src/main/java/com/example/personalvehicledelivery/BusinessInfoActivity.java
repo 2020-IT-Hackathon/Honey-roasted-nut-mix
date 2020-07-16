c02cbc0e8e14432af9a0035a4ab7a8f159e9dc9c
Michael Mao <jmao@gatech.edu>
2020-07-16 16:25:51 -0400

    Re-structured database.

--- PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/BusinessInfoActivity.java
+++ PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/BusinessInfoActivity.java
@@ -33,7 +33,7 @@ public class BusinessInfoActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
-        mDatabase = database.getReference(USER);
+        mDatabase = database.getReference();
 
         editName = findViewById(R.id.editName);
         editRestaurantName = findViewById(R.id.editRestaurantName);
@@ -57,13 +57,20 @@ public class BusinessInfoActivity extends AppCompatActivity {
                 String state = spinnerState.getSelectedItem().toString();
 
                 String uid = mAuth.getCurrentUser().getUid();
-                mDatabase.child(uid).child("full_name").setValue(fullName);
-                mDatabase.child(uid).child("zip").setValue(zip);
-                mDatabase.child(uid).child("business_name").setValue(restaurantName);
-                mDatabase.child(uid).child("street_address").setValue(streetAddress);
-                mDatabase.child(uid).child("city").setValue(city);
-                mDatabase.child(uid).child("business_phone").setValue(businessPhone);
-                mDatabase.child(uid).child("state").setValue(state);
+                mDatabase.child(USER).child(uid).child("full_name").setValue(fullName);
+                mDatabase.child(USER).child(uid).child("zip").setValue(zip);
+                mDatabase.child(USER).child(uid).child("business_name").setValue(restaurantName);
+                mDatabase.child(USER).child(uid).child("street_address").setValue(streetAddress);
+                mDatabase.child(USER).child(uid).child("city").setValue(city);
+                mDatabase.child(USER).child(uid).child("business_phone").setValue(businessPhone);
+                mDatabase.child(USER).child(uid).child("state").setValue(state);
+
+                String userEmail = mAuth.getCurrentUser().getEmail();
+                String userEmailKey = userEmail.replace('.', ',');
+                mDatabase.child("business owners").child(userEmailKey).child("business_name").setValue(restaurantName);
+                mDatabase.child("business owners").child(userEmailKey).child("address").setValue(streetAddress);
+                mDatabase.child("business owners").child(userEmailKey).child("city").setValue(city);
+                mDatabase.child("business owners").child(userEmailKey).child("zip").setValue(zip);
 
                 finish();
                 startActivity(new Intent(BusinessInfoActivity.this, BusinessMainActivity.class));
