e66b170f77976bf66f14a7617d71461822c30cfa
Oreoluwa Olukotun <30515157+Oreosss@users.noreply.github.com>
2020-07-16 10:32:37 -0400

    Add files via upload

--- PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/businessOwner.java
+++ PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/businessOwner.java
@@ -1,4 +1,50 @@
 package com.example.personalvehicledelivery;
 
+
+
 public class businessOwner {
+    private String Name;
+    private String restaurantName;
+    private String streetAddress;
+    private String businessPhone;
+    private String employeeID;
+    private String State;
+
+//constructor
+    public businessOwner(){
+
+    }
+
+    public businessOwner(String Name, String restaurantName, String streetAddress, String businessPhone,String employeeID,String State ){
+        this.Name = Name;
+        this.restaurantName = restaurantName;
+        this.streetAddress = streetAddress;
+        this.businessPhone = businessPhone;
+        this.employeeID = employeeID;
+        this.State = State;
+    }
+
+    public String getName() { return Name; }
+
+    public String getRestaurantName() { return restaurantName; }
+
+    public String getStreetAddress() { return streetAddress; }
+
+    public String getBusinessPhone() { return businessPhone; }
+
+    public String getEmployeeID() { return employeeID; }
+
+    public String getState() {return State; }
+
+    public void setName(String name) { Name = name; }
+
+    public void setRestaurantName(String restaurantName) { this.restaurantName = restaurantName; }
+
+    public void setStreetAddress(String streetAddress) { this.streetAddress = streetAddress; }
+
+    public void setEmployeeID(String employeeID) { this.employeeID = employeeID; }
+
+    public void setBusinessPhone(String businessPhone) { this.businessPhone = businessPhone; }
+
+    public void setState(String state) { State = state; }
 }
