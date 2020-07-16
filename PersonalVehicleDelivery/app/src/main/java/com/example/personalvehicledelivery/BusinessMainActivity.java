bd44e0183c87f2bde89c3d0c3e23b69accb014a2
Timmy Perrine <tnomatt@yahoo.com>
2020-07-16 18:17:59 -0400

    fixed up and added some new stuff
    added help button and dynamic image

--- PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/BusinessMainActivity.java
+++ PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/BusinessMainActivity.java
@@ -4,11 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.renderscript.Sampler;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -26,6 +30,9 @@ public class BusinessMainActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     TextView textViewTitle;
     Spinner spinnerMenu;
+    Button help_btn;
+    TextView help_txt;
+    ImageView comp_logo;
 
     private static final String USERS = "users";
 
@@ -33,6 +40,9 @@ public class BusinessMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_main);
+        help_btn = (Button)findViewById(R.id.help_button);
+        help_txt = (TextView)findViewById(R.id.help_text);
+        comp_logo = (ImageView)findViewById(R.id.comp_logo);
 
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
@@ -44,17 +54,28 @@ public class BusinessMainActivity extends AppCompatActivity {
         textViewTitle = findViewById(R.id.textViewTitle);
         spinnerMenu = findViewById(R.id.spinnerMenu);
 
+        help_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                help_txt.setVisibility((help_txt.getVisibility() == View.VISIBLE)
+                        ? View.GONE : View.VISIBLE);
+            }
+        });
+
+
         ValueEventListener valueEventListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 String businessName = snapshot.child("business_name").getValue().toString();
                 textViewTitle.setText("Welcome, " + businessName);
                 if (businessName.equals("Chick-fil-A")) {
+                    comp_logo.setImageResource(R.drawable.cfa);
                     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(BusinessMainActivity.this,
                             R.array.cfa_menu, android.R.layout.simple_spinner_item);
                     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                     spinnerMenu.setAdapter(adapter);
                 } else if (businessName.equals("Burger King")) {
+                    comp_logo.setImageResource(R.drawable.burger_king_logo);
                     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(BusinessMainActivity.this,
                             R.array.bk_menu, android.R.layout.simple_spinner_item);
                     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
