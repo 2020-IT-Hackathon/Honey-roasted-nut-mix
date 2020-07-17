36ada6b25f0df529c52e46e7ec688707998c9af5
Michael Mao <jmao@gatech.edu>
2020-07-16 21:23:04 -0400

    Added routing functionality.

--- PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/DriverMainActivity.java
+++ PersonalVehicleDelivery/app/src/main/java/com/example/personalvehicledelivery/DriverMainActivity.java
@@ -13,11 +13,25 @@ import android.view.View;
 import android.widget.Button;
 
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class DriverMainActivity extends AppCompatActivity {
+public class DriverMainActivity extends AppCompatActivity implements OnMapReadyCallback, TaskLoadedCallback {
 
     private FirebaseAuth mAuth;
+    GoogleMap map;
+    Button buttonGetDirection;
+
+    MarkerOptions place1, place2;
+    Polyline currentPolyline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,23 @@ public class DriverMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_driver_main);
 
         mAuth = FirebaseAuth.getInstance();
+
+        buttonGetDirection = findViewById(R.id.buttonGetDirection);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFrag);
+        mapFragment.getMapAsync(this);
+
+        //(33.784704, -84.407949)
+        place1 = new MarkerOptions().position(new LatLng(33.784704, -84.407949)).title("Origin");
+        //(33.952890, -84.365042)
+        place2 = new MarkerOptions().position(new LatLng(33.952890, -84.365042)).title("Destination");
+
+        buttonGetDirection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = getUrl(place1.getPosition(), place2.getPosition(), "driving");
+                new FetchURL(DriverMainActivity.this).execute(url, "driving");
+            }
+        });
 
         Button buttonLogout = findViewById(R.id.buttonLogout);
         buttonLogout.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +68,17 @@ public class DriverMainActivity extends AppCompatActivity {
         });
     }
 
+    private String getUrl(LatLng origin, LatLng dest, String dirMode) {
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        String mode = "mode=" + dirMode;
+        String params = str_origin + "&" + str_dest + "&" + mode;
+        String output = "json";
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" +
+                params + "&key=" + getString(R.string.api_key);
+        return url;
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -45,5 +87,21 @@ public class DriverMainActivity extends AppCompatActivity {
             finish();
             startActivity(new Intent(this, MainActivity.class));
         }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.addMarker(place1);
+        map.addMarker(place2);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(33.784704, -84.407949), 12));
+    }
+
+    @Override
+    public void onTaskDone(Object... values) {
+        if (currentPolyline != null) {
+            currentPolyline.remove();
+        }
+        currentPolyline = map.addPolyline((PolylineOptions) values[0]);
     }
 }
